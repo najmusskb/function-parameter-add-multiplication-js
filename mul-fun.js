e3fdd91d82ef0edb7f93b0ec75201c0500575c88
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-03 16:28:29 +0600

    first commit

---
+++ mul-fun.js
@@ -0,0 +1,71 @@
+function MultiplicationTwoNumbers(num1, num2) {
+
+    console.log(MultiplicationTwoNumbers);
+
+    var result = num1 * num2;
+
+    return result;
+}
+
+
+var total = MultiplicationTwoNumbers(2, 3); {
+
+    console.log(total);
+}
+
+
+
+
+
+
+
+
+
+
+function people(p1, p2) {
+    console.log('aje sobaike biriyani deya hobe');
+
+    var totalPeople = p1 / p2;
+    return totalPeople;
+}
+
+var totalpeopleNumber = people(100, 4); {
+    console.log(totalpeopleNumber)
+}
+
+
+
+
+
+function people(p1, p2) {
+    console.log('aje sobaike biriyani deya hobe');
+
+    var totalPeople = p1 + p2;
+    return totalPeople;
+}
+
+var totalpeopleNumber = people(100, 4); {
+    console.log(totalpeopleNumber);
+}
+
+
+
+
+
+
+
+
+function people(p1, p2) {
+    console.log('aje sobaike biriyani deya hobe');
+
+    var totalPeople = p1 - p2;
+    return totalPeople;
+}
+
+var totalpeopleNumber = people(100, 4); {
+    console.log(totalpeopleNumber)
+}
+
+
+
+
---
+++ multiple-parameter.js
@@ -0,0 +1,24 @@
+// multiple parameter add 
+
+
+function addTwoNumbers(number1, number2) {
+
+    console.log(number1, number2);
+
+    var totaladdition = number1 + number2;
+
+    return totaladdition;
+
+}
+
+
+
+var firstNumber = 35;
+
+var secondNumber = 25;
+
+var result = addTwoNumbers(firstNumber, secondNumber);
+
+{
+    console.log('final additional Reasult:', result);
+}
